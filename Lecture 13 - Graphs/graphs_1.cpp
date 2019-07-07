d0133509eab465f6eefe7110b0971107353b7af9
prateek27 <prateeknarang111@gmail.com>
2019-07-07 18:51:38 +0530

    added code

--- Lecture 13 - Graphs/graphs_1.cpp
+++ Lecture 13 - Graphs/graphs_1.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<list>
+#include<queue>
 using namespace std;
 
 class Graph{
@@ -17,11 +18,25 @@ public:
 		if(bidir){
 			l[j].push_back(i);
 		}
-	}	
-	void traverse(int start){
-		
-		
 	}
+	void traverseHelper(int s,bool *visited){
+		cout<<s<<" ";
+		visited[s] = true;
+
+		//visit the neighbours of s and thier neighbours recursilvely
+		for(int nbr:l[s]){
+			if(!visited[nbr]){
+				traverseHelper(nbr,visited);
+			}
+		}
+		return;
+	}
+	//DFS - Depth First Search O(V+E) Linear
+	void traverse(int start){
+		bool *visited = new bool[V]{0};
+		traverseHelper(start,visited);
+	}
+
 	void printAdjList(){
 		//Print the adj list
 		for(int i=0;i<V;i++){
@@ -32,11 +47,62 @@ public:
 			cout<<endl;
 		}
 	}
+	//Level Order Traversal - Breadth First Search(BFS) --O(V+E)
+	void levelOrderTraversal(int s,int d){
+		queue<int> q;
+		bool *visited  = new bool[V]{0};
+		int *dist = new int[V]{0};
+		int *parent = new int[V];
+
+		for(int i=0;i<V;i++){
+			parent[i] = -1;
+		}
+
+		q.push(s);
+		visited[s] = true;
+		dist[s] = 0;
+
+		while(!q.empty()){
+			int f = q.front();
+			q.pop();
+			cout<<f<<",";
+			//for every nbr_node which is there in adjList of f
+			for(int nbr:l[f]){
+				if(!visited[nbr]){
+					q.push(nbr);
+					visited[nbr] = true;
+					dist[nbr] = dist[f] + 1;
+					parent[nbr] = f;
+				}
+			}
+		} 
+
+		cout<<"Shortest Path btw src and dest"<<dist[d]<<endl;
+		cout<<"Path ";
+		int t = d;
+		while(t!=-1){
+			cout<<t<<"<---";
+			t = parent[t];
+		}
+	}
 };
 
 
 
 int main(){
+
+	Graph g(6);
+	g.addEdge(0,1);
+	g.addEdge(0,4);
+	g.addEdge(2,1);
+	g.addEdge(3,4);
+	g.addEdge(4,5);
+	g.addEdge(2,3);
+	g.addEdge(3,5);
+
+	g.levelOrderTraversal(0,5);
+
+	/*
 	Graph g(4);
 	g.addEdge(0,1);
 	g.addEdge(1,2);
@@ -44,6 +110,11 @@ int main(){
 	g.addEdge(2,3);
 
 	g.printAdjList();
+	cout<<endl;
+
+	g.traverse(0);
+	g.levelOrderTraversal(0);
+	*/
 
 	
 
